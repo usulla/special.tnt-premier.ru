1a3550d7dbee6b13a9b3845fe143209679c2f080
Andrew Dyachenko <north.inhale@gmail.com>
2018-12-17 17:46:25 +0300

    Extending with subscribe
    Extending with subscribe functionality

--- src/components/EmailForm/EmailForm.js
+++ src/components/EmailForm/EmailForm.js
@@ -2,33 +2,42 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './EmailForm.scss'
 
-const EmailForm = ({ children, className, buttonText, submitHandler }) =>
-	<form className={className} onSubmit={submitHandler}>
-		<input type='email' name='email' placeholder='Email' className={`${className}__field`} required />
-		<div className={`${className}__group ${className}__group--checkbox`}>
-			<input
-				type='checkbox'
-				name='subscribe-permit'
-				id='subscribe-permit'
-				className={`visibility-hidden ${className}__checkbox`}
-				required />
-			<label htmlFor='subscribe-permit' className={`${className}__checkbox ${className}__checkbox--fake`} />
-			<label htmlFor='subscribe-permit' className={`${className}__label`}>
-				Согласен получать информационные рассылки, в том числе рекламные и иные материалы ООО «ГПМ РТВ» и третьих лиц
-			</label>
-		</div>
-		<button type='submit' className={`${className}__submit`}>
-			{buttonText}
-		</button>
-		{
-			children ?
-				children :
-				null
-		}
-	</form>
+const EmailForm = ({ children, subscribed, className, buttonText, submitHandler }) =>
+	(subscribed === true ?
+		<div className={`${className}__callout`}>
+			Ваш Email успешно <span className={`${className}__callout-success`}>отправлен!</span>
+		</div> :
+		subscribed === false ?
+			<div className={`${className}__callout`}>
+				Указанный вами Email <span className={`${className}__callout-fail`}>отклонён!</span>
+			</div> :
+			<form className={className} onSubmit={submitHandler}>
+				<input type='email' name='email' placeholder='Email' className={`${className}__field`} required />
+				<div className={`${className}__group ${className}__group--checkbox`}>
+					<input
+						type='checkbox'
+						name='subscribe-permit'
+						id='subscribe-permit'
+						className={`visibility-hidden ${className}__checkbox`}
+						required />
+					<label htmlFor='subscribe-permit' className={`${className}__checkbox ${className}__checkbox--fake`} />
+					<label htmlFor='subscribe-permit' className={`${className}__label`}>
+						Согласен получать информационные рассылки, в том числе рекламные и иные материалы ООО «ГПМ РТВ» и третьих лиц
+					</label>
+				</div>
+				<button type='submit' className={`${className}__submit`}>
+					{buttonText}
+				</button>
+				{
+					children ?
+						children :
+						null
+				}
+			</form>)
 
 EmailForm.defaultProps = {
 	children: null,
+	subscribed: null,
 	className: 'email-form',
 	buttonText: '',
 	submitHandler: f => f
@@ -39,6 +48,10 @@ EmailForm.propTypes = {
 		PropTypes.arrayOf(PropTypes.node),
 		PropTypes.node,
 		PropTypes.string
+	]),
+	subscribed: PropTypes.oneOfType([
+		PropTypes.oneOf([null]),
+		PropTypes.bool
 	]),
 	className: PropTypes.string.isRequired,
 	buttonText: PropTypes.string,
