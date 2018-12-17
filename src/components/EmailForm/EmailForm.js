a1d707a45c3d531e406b6289184eb62a56627e6b
Andrew Dyachenko <north.inhale@gmail.com>
2018-12-17 15:12:43 +0300

    Email form checkbox
    Adding checkbox styles and logics

--- src/components/EmailForm/EmailForm.js
+++ src/components/EmailForm/EmailForm.js
@@ -2,22 +2,47 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './EmailForm.scss'
 
-const EmailForm = ({ className, buttonText, submitHandler }) =>
+const EmailForm = ({ children, className, buttonText, submitHandler }) =>
 	<form className={className} onSubmit={submitHandler}>
-		<input type='email' placeholder='Email' className={`${className}__field`} required />
+		<input type='email' name='email' placeholder='Email' className={`${className}__field`} required />
+		<div className={`${className}__group ${className}__group--checkbox`}>
+			<input
+				type='checkbox'
+				name='subscribe-permit'
+				id='subscribe-permit'
+				className={`visibility-hidden ${className}__checkbox`}
+				required />
+			<label htmlFor='subscribe-permit' className={`${className}__checkbox ${className}__checkbox--fake`} />
+			<label htmlFor='subscribe-permit' className={`${className}__label`}>
+				Согласен получать информационные рассылки, в том числе рекламные и иные материалы ООО «ГПМ РТВ» и третьих лиц
+			</label>
+		</div>
 		<button type='submit' className={`${className}__submit`}>
 			{buttonText}
 		</button>
+		{
+			children ?
+				children :
+				null
+		}
 	</form>
 
-EmailForm.propTypes = {
-	className: PropTypes.string.isRequired,
-	buttonText: PropTypes.string
+EmailForm.defaultProps = {
+	children: null,
+	className: 'email-form',
+	buttonText: '',
+	submitHandler: f => f
 }
 
-EmailForm.defaultProps = {
-	className: 'email-form',
-	buttonText: ''
+EmailForm.propTypes = {
+	children: PropTypes.oneOfType([
+		PropTypes.arrayOf(PropTypes.node),
+		PropTypes.node,
+		PropTypes.string
+	]),
+	className: PropTypes.string.isRequired,
+	buttonText: PropTypes.string,
+	submitHandler: PropTypes.func
 }
 
 export default EmailForm
