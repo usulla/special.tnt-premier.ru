60992d4f90cdddf0e5f63893651e52e82e4a0fa2
Andrew Dyachenko <north.inhale@gmail.com>
2018-12-18 17:42:18 +0300

    Unnesesary import

--- src/components/Sharing/Sharing.js
+++ src/components/Sharing/Sharing.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './Sharing.scss'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import {
     TwitterShareButton,
